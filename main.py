fa25aa2a06303e6006074efb2dd7219a6d5c2d86
AndShaA <andrey.shevchenko.03@mail.ru>
2023-03-30 17:51:05 +0500

    Сделал домашнюю страницу при загрузке

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socket
 def start_my_server():
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('127.0.0.1',8000))
+        server.bind(('127.0.0.1', 8000))
 
         server.listen(4)
 
@@ -26,15 +26,19 @@ def load_page_from_get_request(request_data):
     HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
     HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset = utf-8\r\n\r\n'
     path = request_data.split(' ')[1]
+
     response = ''
     try:
-        with open('views'+path, 'rb') as file:
-            response = file.read()
+        if path == '/':
+            with open('views/index.html', 'rb') as file:
+                response = file.read()
+        else:
+            with open('views' + path, 'rb') as file:
+                response = file.read()
         return HDRS.encode('utf-8') + response
     except FileNotFoundError:
+        print('Not found')
         return (HDRS_404 + 'Sorry, bro! No PAge...').encode('utf-8')
-
-#client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if __name__ == '__main__':
     start_my_server()
